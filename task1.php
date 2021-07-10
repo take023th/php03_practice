b80d637da8f12dc00ae4afb7787a7a3c3b7d9c0f
TakeakiHirasawa <take38ugj441@aol.com>
2021-07-10 14:51:33 +0000

    php03_practice

---
+++ task1.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Takeaki Hirasawa";
+
+if($name = "Takeaki Hirasawa" ){
+    echo '「私はあなたの名前です」';
+}else{
+    echo '「あなたの名前ではありません';
+}
+?>
---
+++ task3.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array('リンゴ','メロン','ブルーベリー','ドラゴンフルーツ',"みかん");
+
+foreach($fruits as $value){
+    echo $value;
+}

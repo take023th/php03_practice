7c5897c89af2911fbd1edd923b38e0466ea4bd0e
TakeakiHirasawa <take38ugj441@aol.com>
2021-07-13 09:43:21 +0000

    retext

--- task1.php
+++ task1.php
@@ -1,9 +1,13 @@
 <?php
 $name = "Takeaki Hirasawa";
 
-if($name = "Takeaki Hirasawa" ){
-    echo '「私はあなたの名前です」';
+if($name == "Takeaki Hirasawa"){
+    echo "「私ははあなたの名前です」";
 }else{
-    echo '「あなたの名前ではありません';
+    echo "「あなたの名前ではありません」";
 }
+
+//修正しました。比較演算子は==でした
+//前のechoとelseの間に空白のラインができていました。
+//完全に動作することを確認しました。
 ?>
--- task2.php
+++ task2.php
@@ -1,5 +1,8 @@
 <?php
-for ($i = 1;$i <= 10000;$i++){
-    echo $i;
+$total = 0;
+for ($i = 0;$i <= 10000;$i++){
+    $total += $i;
 }
+echo $total;
+//課題を勘違いしていました。
 ?>
